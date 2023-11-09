f00e53391405e39898d7d571404cf151f01f44c4
Nachtmann <xnachtmann@stuba.sk>
2023-11-09 16:42:58 +0100

    dokument bug fix

--- js/script.js
+++ js/script.js
@@ -1,20 +1,51 @@
 $('#exampleModal').on('show.bs.modal', function (event) {
-  var button = $(event.relatedTarget); // Button that triggered the modal
-  var recipient = button.data('whatever'); // Extract info from data-* attributes
+  var modal = $(this);
+  var button = $(event.relatedTarget);
+  var recipient = button.data('whatever');
   var recipientImage = '';
+  var recipientContent = '';
 
-  // Determine the image source based on the recipient's name
-  if (recipient === 'Diagram1') {
-    recipientImage = '../images/diagram1.jpg';
+  if (recipient === 'JSON') {
+    var jsonData = {
+      "Price": "$2.50",
+      "Configuration": "Single",
+      "V(BR)DSS Min (V)": "900",
+      "RDS(on) @ VGS = 10 V (mΩ)†": "0.05",
+      "ID Max (A)": "100",
+      "Qg @ VGS = 10 V (nC)†": "50",
+      "VGS Max (V)": "20",
+      "Technology": "MOSFET",
+      "Package Type": "TO-220",
+      "Ciss Typ (pF)": "1000",
+      "VGS(th) Max (V)": "5",
+      "PD Max (W)": "150",
+      "RDS(on) Max @ VGS = 2.5 V  (mΩ)": "0.1",
+      "RDS(on) Max @ VGS = 4.5 V  (mΩ)": "0.08",
+      "Qg Typ @ VGS = 4.5 V (nC)": "40",
+      "Coss Typ (pF)": "500",
+      "Crss Typ (pF)": "200",
+      "Qgd Typ @ VGS = 4.5 V (nC)": "30",
+      "Qrr Typ (nC)": "10",
+      "Qualification": "Automotive",
+      "Tj Max (°C)": "150",
+      "AEC Qualified": true,
+      "PPAP Capable": true,
+      "Halide Free": true,
+      "Lead Free": true,
+      "Status": "Active"
+    };
+
+    recipientImage = '';
+    recipientContent = '<pre>' + JSON.stringify(jsonData, null, 2) + '</pre>';
+  } else if (recipient === 'Diagram1') {
+    recipientContent = '<img id="recipient-image" src="../images/diagram1.jpg" alt="Diagram 1 class="img-fluid"">';
   } else if (recipient === 'Diagram2') {
-    recipientImage = '../images/diagram2.jpg';
+    recipientContent = '<img id="recipient-image" src="../images/diagram2.jpg" alt="Diagram 2" class="img-fluid">';
   } else if (recipient === 'Parsovanie Do JSON') {
-    recipientImage = '../images/parsovanieDoJSON.jpg';
+    recipientContent = '<img id="recipient-image" src="../images/parsovanieDoJSON.jpg" alt="Parsovanie Do JSON" class="img-fluid">';
   }
 
-  // Update the modal's content, including the image source
-  var modal = $(this);
   modal.find('.modal-title').text(recipient);
-  modal.find('#recipient-image').attr('src', recipientImage);
+  modal.find('.modal-body').html(recipientContent);
   modal.find('.modal-body input').val(recipient);
 });
